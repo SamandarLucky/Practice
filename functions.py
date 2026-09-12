681424cef7030151631875f9bf00b33c5fd22b8e
Samandar(Sam) Turdaliev <abc@abcs-MacBook-Air.local>
2026-09-12 08:09:02 -0700

    fix: scope and priority

--- functions.py
+++ functions.py
@@ -38,3 +38,15 @@ print("result3:", result3) #source yanada aniqroq
 
 result4 = give_greet("John")
 print("result4:", result4) 
+
+print("===== Scope =====")
+b = 100 
+
+
+# DEFINE
+def calculate(a):
+    c = a + b
+    print(f"the c value: {c}")
+
+# CALL
+calculate(5)

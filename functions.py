fc0b48f16057a380cc52d81c00a84503f2d0ecd4
Samandar(Sam) Turdaliev <abc@abcs-MacBook-Air.local>
2026-09-12 08:00:16 -0700

    fix: learn keyword & default argument

--- functions.py
+++ functions.py
@@ -11,7 +11,7 @@ print("===== DEFINE & CALL =====")
 # Instead of block {} in JAVA, Python uses indentation!
 
 
-# DEFINE - build qismi
+# DEFINE - build qismi - parametr
 def greet(a):
     print(f"How do you do, {a}")
 
@@ -20,9 +20,21 @@ def greeting(b):
     print("greeteing is executed")
     return f"Hi {b}"
 
-# CALL - execute
+# CALL - execute - argument
 result1 = greet('Martin')
 print("result1:", result1)
 
 result2 = greeting("Samandar")
 print("result2:", result2)
+
+print("===== Keyword & Default arguments =====")
+# DEFINE
+def give_greet(name, age=22):
+    print("give greet is executed")
+    return f"Hi {name}, you are {age} years old!"
+
+result3 = give_greet(name="Samandar", age=24)
+print("result3:", result3) #source yanada aniqroq
+
+result4 = give_greet("John")
+print("result4:", result4) 

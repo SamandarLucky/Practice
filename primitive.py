3dca88a62a5f029604dfe821f351a218f6b0cb84
Samandar(Sam) Turdaliev <abc@abcs-MacBook-Air.local>
2026-09-12 07:15:16 -0700

    feat: learn string in python

--- primitive.py
+++ primitive.py
@@ -10,3 +10,20 @@ print(f"the count: {count} and type: {count_type}")
 result1 = count.bit_count() #method
 result2 = count.numerator # state
 print(result1, result2)
+
+print("========= string ==========") 
+#METHODS: upper() lower() title() find() replace()
+
+# string lar primitive variable va state, method lariga ega ekan
+course = "AI Python Fullstack"
+result = type(course)
+print(f"the result (1): {result}")
+result = course.title() #bosh harflarini Katta qilib beradi
+print(f"the result (2): {result}")
+
+result = course.upper() #upper case boldi
+print(f"the result (3): {result}")
+
+result = course.replace("Fullstack", "MasterClass")
+print(f"the result (4): {result}")
+print(course)
